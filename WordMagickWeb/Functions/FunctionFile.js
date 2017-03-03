26fec599559ba8cfdf68e6398d771871d3548e55
RovingStone <sergey@matveev.pp.ru>
2017-03-03 21:16:36 +0300

    Add basic matrix processing, cleanup code

--- WordMagickWeb/Functions/FunctionFile.js
+++ WordMagickWeb/Functions/FunctionFile.js
@@ -1,10 +1,9 @@
-﻿// Функцию инициализации необходимо выполнять при каждой загрузке новой страницы.
-(function () {
+﻿(function () {
     "use strict"
 
     Office.initialize = function (reason) {
         $(document).ready(function () {
-            console.log("initialize");
+            console.info("Initialize completed");
         });
     }
 })();
@@ -21,39 +20,32 @@ function shuffle(a) {
     return a;
 }
 
-function consoleLogHandler(data) {
-    console.log(data);
-}
-
 function asyncResultHandler(asyncResult) {
     if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
-        console.log("Async OK!");
+        console.info("asyncResult.status == Succeeded");
     }
     else {
-        console.log("Async error: " + asyncResult.error.message);
+        console.error("asyncResult.status == Failed. Message is: " + asyncResult.error.message);
     }
 }
 
 function makeSelectedDataAsyncHandler(coercionType) {
     function asyncHandler() {
-        console.log("Start handler");
         Office.context.document.getSelectedDataAsync(
             coercionType, function (asyncResult) {
                 if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
-                    // normal processing
-                    console.log("Normal processing");
+                    console.info("asyncHandler - execute success handler");
                     result = asyncHandler.successHandler(asyncResult.value);
                     Office.context.document.setSelectedDataAsync(result, asyncResultHandler);
                 }
                 else {
-                    // something wrong, lets try fallback
-                    console.log("Fallback processing");
+                    console.info("asyncHandler - execute fallback handler");
                     asyncHandler.fallbackHandler(asyncResult.error.message);
                 }
             });
     }
-    asyncHandler.successHandler = console.log;
-    asyncHandler.fallbackHandler = console.log;
+    asyncHandler.successHandler = console.info;
+    asyncHandler.fallbackHandler = console.warn;
     return asyncHandler;
 }
 
@@ -62,55 +54,26 @@ function changeSelectedDataAsync(changer) {
     matrixHandler = makeSelectedDataAsyncHandler(Office.CoercionType.Matrix);
 
     textHandler.successHandler = changer;
-    matrixHandler.successHandler = changer;
+    matrixHandler.successHandler = function (matrix) {
+        return matrix.map(function (e) {
+            if (Array.isArray(e)) {
+                return e.map(changer);
+            }
+            else {
+                console.warn(e);
+                return e;
+            }
+        });
+    }
     matrixHandler.fallbackHandler = textHandler;
 
+    // Try matrix handler first, because matrix can be converted to text, but not vice versa
     matrixHandler();
 }
 
 function shuffle_chars() {
     changeSelectedDataAsync(function (data) {
-        console.log(data);
+        console.info("changer arg: " + data);
         return data;
     });
-}
-
-function temp() {
-    Word.run(function (context) {
-        var range = context.document.getSelection();
-        context.load(range, 'text');
-        return context.sync()
-            .then(function () {
-                var result = [];
-                var words = range.text.split(/\s+/);
-                words.forEach(function (el, index, array) {
-                    word = el.slice(1, -1).split('');
-                    word = shuffle(word).join('');
-                    console.log("" + el + " -> " + el[0] + word + el[el.length - 1]);
-                    array[index] = el[0] + word + el[el.length - 1];
-                    return true;
-                });
-                res = words.join(' ');
-                Office.context.document.setSelectedDataAsync(res, function (asyncResult) {
-                    if (asyncResult.status == Office.AsyncResultStatus.Failed) {
-                        console.log(asyncResult.error.message);
-                    }
-                });
-            })
-            .then(context.sync)
-            .then(function () {
-                //
-            })
-            .then(context.sync)
-    })
-    .catch(errorHandler);
-}
-
-//$$(Helper function for treating errors, $loc_script_taskpane_home_js_comment34$)$$
-function errorHandler(error) {
-    // $$(Always be sure to catch any accumulated errors that bubble up from the Word.run execution., $loc_script_taskpane_home_js_comment35$)$$
-    console.log("Error: " + error);
-    if (error instanceof OfficeExtension.Error) {
-        console.log("Debug info: " + JSON.stringify(error.debugInfo));
-    }
 }
